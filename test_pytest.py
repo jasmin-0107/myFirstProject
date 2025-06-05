018f8f131de2eb02d7e8d01744f28238a6f2b8f1
jasmin-0107 <hxiang63@gmail.com>
2025-06-05 21:56:32 +0200

    functions for pytest

--- test_pytest.py
+++ test_pytest.py
@@ -1,22 +1,8 @@
-import unittest
-import inc_dec  # 被测试的代码
+def add(a, b):
+    """Diese Funktion addiert zwei Zahlen."""
+    return a + b
 
-class TestIncrementDecrement(unittest.TestCase):
-    def test_increment(self):
-        self.assertEqual(inc_dec.increment(3), 4)
+def test_add():
+    """Testet die add Funktion."""
+    assert add(2, 3) == 5
 
-    def test_decrement(self):
-        self.assertEqual(inc_dec.decrement(3), 2)
-
-    def test_increment_zero(self):
-        self.assertEqual(inc_dec.increment(0), 1)
-
-    def test_decrement_zero(self):
-        self.assertEqual(inc_dec.decrement(0), -1)
-
-    def test_invalid_input(self):
-        with self.assertRaises(TypeError):
-            inc_dec.increment("a")
-
-if __name__ == "__main__":
-    unittest.main()
